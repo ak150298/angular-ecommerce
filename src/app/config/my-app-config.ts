78ad934a30797d80a91155dc2ebe5d8a28c818b3
Ayush Kansal <ayushmails070@gmail.com>
2023-11-08 21:34:36 +0530

    Added security

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+    oidc: {
+        clientId: '0oad60aw7fvqygNBc5d7',
+        issuer: 'https://dev-79139316.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: [ 'openid','profile','email' ]
+    }
+}
+
